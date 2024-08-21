215eee81c046dc2ca2d8e2558bf344d9d62c66f6
TML104 <1041159637@qq.com>
2024-08-21 21:29:02 +0800

    从命令行读

--- STLNonManifold/main.cpp
+++ STLNonManifold/main.cpp
@@ -2,10 +2,11 @@
 
 #include "STLNonManifold.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    STLNonManifold::STLNonManifoldChecker stlNonManifoldChecker("./stl_models/zhu.stl");
+    //STLNonManifold::STLNonManifoldChecker stlNonManifoldChecker("./stl_models/zhu.stl");
     //STLNonManifold::STLNonManifoldChecker stlNonManifoldChecker("./stl_models/Stanford_Bunny_sample.stl");
-    
+
+    STLNonManifold::STLNonManifoldChecker stlNonManifoldChecker(argv[1]);
     stlNonManifoldChecker.CheckNonManifold();
 }
